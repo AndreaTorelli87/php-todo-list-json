7c4a53acdfd293b6b6a0f4d14d1cca57235f7973
Andrea Torelli <gioran326@protonmail.com>
2023-04-20 16:55:53 +0200

    Fine

--- main.js
+++ main.js
@@ -3,13 +3,29 @@ const { createApp } = Vue
 createApp({
    data() {
       return {
-         
+         todoList: [],
+         todo: ""
       }
    },
    methods: {
-      
+      importaLista() {
+         axios.get("server.php")
+            .then(response => this.todoList = response.data)
+      },
+      addTodo() {
+         const data = {
+            input: this.todo
+         };
+
+         axios.post("server.php", data,
+            { headers: { "Content-Type": "multipart/form-data" }}
+         ).then(response => {
+            this.todoList = response.data;
+            this.todo = "";
+         });
+      }
    },
    mounted() {
-
+      this.importaLista()
    }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,2 +1,17 @@
 <?php
 
+$todoList = [
+   "HTML",
+   "CSS",
+   "Responsive",
+   "JS",
+   "Vue",
+   "php"
+];
+
+if (isset($_POST["input"])) {
+   $todoList[] = $_POST["input"];
+}
+
+header("Content-Type: application/json");
+echo json_encode($todoList);
